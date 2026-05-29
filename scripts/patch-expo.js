9ceb6ba6d315e0953fe19ee7b66cb2a5bd2f03bd
Juliana Assis <julianaaas404@gmail.com>
2026-05-29 13:08:53 -0300

    fix: use JVM target 17 for expo-font patch to match GitHub Actions JDK 17

--- scripts/patch-expo.js
+++ scripts/patch-expo.js
@@ -37,11 +37,11 @@ android {
         versionName "56.0.5"
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_11
-        targetCompatibility JavaVersion.VERSION_11
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = '11'
+        jvmTarget = '17'
     }
 }
 
